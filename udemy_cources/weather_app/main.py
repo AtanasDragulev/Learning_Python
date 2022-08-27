5ba0d6263bb5ba21163ba70f39997881f7cb08be
dragulev <atanas.dragulev@gmail.com>
2022-08-27 21:32:32 +0300

    Bring umbrella reminder
    Simple app that checks if there is rain in the forecast and prints to bring umbrella

---
+++ udemy_cources/weather_app/main.py
@@ -0,0 +1,22 @@
+import requests
+
+lat = 42.698334
+lon = 23.319941
+
+
+part = "current,minutely,daily,alerts"
+api_key = "cf5c6dac7157ff58ab9f8ef1a0f37aa8"
+params = {"lat": lat,
+          "lon": lon,
+          "exclude": part,
+          "appid": api_key
+          }
+api = f"https://api.openweathermap.org/data/2.5/onecall"
+
+request = requests.get(api, params=params)
+request.raise_for_status()
+weather_data = request.json()
+weather_slice = weather_data["hourly"][:12]
+weather = [day["weather"][0]["id"] for day in weather_slice if int(day["weather"][0]["id"]) < 700]
+if len(weather) > 0:
+    print("Bring umbrella")
